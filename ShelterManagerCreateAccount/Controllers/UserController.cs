7c2d7d25c5402204e123901510d1c65a6b0dcba6
Joshua Pichanick <jdPichanick@live.com>
2023-09-25 19:04:03 -0400

    Add project files.

---
+++ ShelterManagerCreateAccount/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class UserController : Controller
+{
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(User model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Here, you would typically save the user to a database or perform user registration logic.
+            // For simplicity, we're not including database interactions in this example.
+
+            return RedirectToAction("Login", "Account"); // Redirect to the login page after successful registration
+        }
+
+        return View(model);
+    }
+}
