0719714a519b20ee4757e9b3aec48c8eeb1aa64e
kellimoore <kellim0619@gmail.com>
2023-11-09 11:53:56 -0500

    Shelter Profile Update

---
+++ ShelterManagerCreateAccount/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShelterManagerCreateAccount.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult EditShelterProfile()
+        {
+            return View();
+        }
+    }
+}
+
